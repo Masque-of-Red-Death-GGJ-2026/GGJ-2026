1ac5e5458614495e2622f06e772495f86c5cf600
CodingANDCoffeeEQHappy <>
2026-01-31 14:22:08 -0600

    Added start, controls, and credits menus.

---
+++ Trash Bandit/Assets/Scripts/Menus/MainMenuScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void DisplayControls()
+    {
+        SceneManager.LoadScene("ControlsScene");
+    }
+
+    public void DisplayCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
