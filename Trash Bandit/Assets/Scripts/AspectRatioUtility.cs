e56b0c2da0ce6b2da27a69ac190c487f5ecab707
James Struble <jas2hf@mail.umsl.edu>
2026-02-26 00:07:53 -0600

    Black bars now updates with aspect ratio changes

--- Trash Bandit/Assets/Scripts/AspectRatioUtility.cs
+++ Trash Bandit/Assets/Scripts/AspectRatioUtility.cs
@@ -4,15 +4,25 @@ using UnityEngine.Rendering.Universal;
 public class AspectRatioUtility : MonoBehaviour
 {
     // Credit to: https://www.youtube.com/watch?v=PClWqhfQlpU
+    private float savedAspectRatio;
     void Start()
     {
         AdjustAspectRatio();
+    }
+
+    void Update()
+    {
+        if (savedAspectRatio != (float)Screen.width / (float)Screen.height)
+        {
+            AdjustAspectRatio();
+        }
     }
 
     void AdjustAspectRatio()
     {
         float targetAspectRatio = 16.0f/9.0f;
         float screenAspectRatio = (float)Screen.width / (float)Screen.height;
+        savedAspectRatio = screenAspectRatio;
         float scaleHeight = screenAspectRatio / targetAspectRatio;
 
         Camera gameCam = GetComponent<Camera>();
