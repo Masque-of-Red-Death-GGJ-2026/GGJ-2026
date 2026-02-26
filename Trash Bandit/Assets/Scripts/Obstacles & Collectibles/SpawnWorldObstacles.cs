8ffae0dfe66bfe78da6b18666e52340f0392f17b
kelgruber <80933498+kelgruber@users.noreply.github.com>
2026-02-26 13:08:11 -0600

    Potential fix for the double collectible spawning bug
    Rewrote CheckForConflict to use Physics.CheckSphere for other collectibles in the same space

--- Trash Bandit/Assets/Scripts/Obstacles & Collectibles/SpawnWorldObstacles.cs
+++ Trash Bandit/Assets/Scripts/Obstacles & Collectibles/SpawnWorldObstacles.cs
@@ -7,6 +7,9 @@ public class SpawnWorldObstacles : MonoBehaviour
     private float spawnCooldownTimer = 0;
     [SerializeField] float spawnCooldownMin;
     [SerializeField] float spawnCooldownMax;
+    
+    [SerializeField] float spawnRadius = 5f;
+    [SerializeField] float checkRadius = 1f; 
 
     void Start()
     {
@@ -31,25 +34,46 @@ public class SpawnWorldObstacles : MonoBehaviour
         return Random.Range(min, max);
     }
 
-    void CheckForConflict()
-    {
-        // Get each obstacle currently in game:
-        foreach (GameObject obstacle in GameObject.FindGameObjectsWithTag("Obstacle"))
-        {
-            // If obstacle not at spawn location and game not over, spawn collectible:
-            if (!(gameObject.transform.position.x == obstacle.transform.position.x) && gameManager.gameOver == false)
-            {
-                SpawnObject(transform.position);
-            } 
-            else
-            {
-                // This spawn position allows some randomness for distance between spawned collectibles:
-                Vector2 spawnPosition = new Vector2(transform.position.x + Random.Range(0.0f, 0.005f), transform.position.y);
+    void CheckForConflict() {
+        Vector3 spawnPosition = Vector3.zero;
+        bool canSpawn = false;
 
-                SpawnObject(spawnPosition);
+        while (!canSpawn && gameManager.gameOver == false) {
+            // choose a new random position
+            Vector2 newPosition = new Vector2(transform.position.x + Random.Range(0.0f, 0.005f), transform.position.y);
+            // Check there are any box colliders within a circle around it, if so keep the position and spawn 
+            if (!Physics.CheckSphere(newPosition, checkRadius)) {
+                canSpawn = true;
+                spawnPosition = newPosition;
             }
+            // if space is not clear then repeat with a new position
+        }
+        // spawn object in clear space
+        if (canSpawn) {
+            SpawnObject(spawnPosition);
         }
     }
+
+    // Original CheckForConflict function, was producing double & triple collectibles in same space
+    // void CheckForConflict()
+    // {
+    //     // Get each obstacle currently in game:
+    //     foreach (GameObject obstacle in GameObject.FindGameObjectsWithTag("Obstacle"))
+    //     {
+    //         // If obstacle not at spawn location and game not over, spawn collectible:
+    //         if (!(gameObject.transform.position.x == obstacle.transform.position.x) && gameManager.gameOver == false)
+    //         {
+    //             SpawnObject(transform.position);
+    //         } 
+    //         else
+    //         {
+    //             // This spawn position allows some randomness for distance between spawned collectibles:
+    //             Vector2 spawnPosition = new Vector2(transform.position.x + Random.Range(0.0f, 0.005f), transform.position.y);
+
+    //             SpawnObject(spawnPosition);
+    //         }
+    //     }
+    // }
 
     void SpawnObject(Vector2 spawnPosition)
     {
